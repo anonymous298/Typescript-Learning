60df88d39bf322e77d47cf63e53161982bec1fe6
anonymous298 <tackletalha@gmail.com>
2025-11-06 12:23:07 +0500

    type assertion and type guard

--- Functional-Programming/app.js
+++ Functional-Programming/app.js
@@ -1,4 +1,19 @@
 //Functions
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // Function types
 // Optional and default parameters
 // Rest parameters
@@ -43,3 +58,41 @@ function generic(value) {
 }
 console.log(generic('tlahha'));
 console.log(generic(23));
+// Type assertion
+var a = '151';
+console.log(typeof Number(a)); // This is type casting
+// Type Guards and TypeScript Utility Types
+// Using typeof and instanceof
+// Partial, Required, Readonly
+function abcde(a) {
+    // type narrowing
+    if (typeof a === 'string') {
+    }
+    else if (typeof a === 'number') {
+    }
+    else {
+        throw new Error('Pagal ho gya kia');
+    }
+}
+// using instanceof
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    Animal.prototype.walk = function () {
+        console.log('ANimal is walking...');
+    };
+    return Animal;
+}());
+var Cat = /** @class */ (function (_super) {
+    __extends(Cat, _super);
+    function Cat() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Cat.prototype.meow = function () {
+        console.log('Cat is meowing...');
+    };
+    return Cat;
+}(Animal));
+var ca = new Cat();
+console.log(ca instanceof Animal);
+console.log(ca instanceof Cat);
--- Functional-Programming/app.ts
+++ Functional-Programming/app.ts
@@ -65,3 +65,48 @@ function generic<T>(value: T): T {
 console.log(generic<string>('tlahha'))
 console.log(generic(23)) 
 
+// Type assertion
+
+let a: string = '151';
+
+console.log(typeof Number(a))  // This is type casting
+
+
+// Type Guards and TypeScript Utility Types
+
+// Using typeof and instanceof
+// Partial, Required, Readonly
+
+function abcde(a: string | number) {
+    // type narrowing
+    if (typeof a === 'string') {
+        
+    }
+
+    else if (typeof a === 'number') {
+        
+    }
+
+    else {
+        throw new Error('Pagal ho gya kia')
+    }
+}
+
+// using instanceof
+
+class Animal {
+    walk() {
+        console.log('ANimal is walking...')
+    }
+}
+
+class Cat extends Animal {
+    meow() {
+        console.log('Cat is meowing...')
+    }
+}
+
+let ca = new Cat()
+
+console.log(ca instanceof Animal)
+console.log(ca instanceof Cat)
